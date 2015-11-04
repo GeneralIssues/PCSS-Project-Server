cbcea662398c246333055c433d01d626e73dcbd0
Oana Dogaru <odogar14@student.aau.dk>
2015-11-04 20:50:45 +0100

    Player class created with constructor and get+set. Needs array of card objects added.

---
+++ Tickets To Ride/src/Player.java
@@ -0,0 +1,40 @@
+
+public class Player {
+	int points;
+	int trains;
+	String name;
+	//insert array of card objects here;
+	//insert array of mission card objects here;
+	
+	public Player(int points, int trains, String name){
+		this.points=points; 
+		this.trains=trains;
+		this.name=name;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
+
+	public int getTrains() {
+		return trains;
+	}
+
+	public void setTrains(int trains) {
+		this.trains = trains;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
