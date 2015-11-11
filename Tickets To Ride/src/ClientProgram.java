bcce3cb8df34dfc258de2298a274210c090f1346
Fred <felmqu14@student.aau.dk>
2015-11-11 15:42:43 +0100

    Create an exmaple Lobby objects and ensured that it can be sent Also comments and small changes.

--- Tickets To Ride/src/ClientProgram.java
+++ Tickets To Ride/src/ClientProgram.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 import com.esotericsoftware.kryonet.Client;
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
@@ -5,30 +7,35 @@ import com.esotericsoftware.kryonet.Listener;
 
 public class ClientProgram extends Listener {
 
-	//Our client object.
+	//The client object.
 	static Client client;
-	//IP to connect to.
-	static String ip = "172.30.211.199";
-	//Ports to connect on.
+	
+	//IP to connect to. !IMPORANT VARIABLE!
+	static String ip = "172.30.211.124";
+	
+	//Ports to connect to.
 	static int udpPort = 4445, tcpPort = 4444;
 	
-	//A boolean value.
+	//Boolean to check if a packet is received
 	static boolean messageReceived = false;
 	
 	public static void main(String[] args) throws Exception {
 		System.out.println("Connecting to the server...");
+		
 		//Create the client.
 		client = new Client();
 		
-		//Register the packet object.
-		client.getKryo().register(PacketMessage.class);
+		//Register different classes.
+		client.getKryo().register(Lobby.class);
+		client.getKryo().register(Player.class);
+		client.getKryo().register(Player[].class);
 
 		//Start the client
 		client.start();
 		//The client MUST be started before connecting can take place.
 		
 		//Connect to the server - wait 5000ms before failing.
-		client.connect(50000, ip, tcpPort, udpPort);
+		client.connect(5000, ip, tcpPort, udpPort);
 		
 		//Add a listener
 		client.addListener(new ClientProgram());
@@ -37,22 +44,21 @@ public class ClientProgram extends Listener {
 		
 		//This is here to stop the program from closing before we receive a message.
 		while(!messageReceived){
+			System.out.println("No messege yet");
 			Thread.sleep(1000);
 		}
-		
+		//This happens when messegeReceived is true
 		System.out.println("Client will now exit.");
 		System.exit(0);
 	}
 	
-	//I'm only going to implement this method from Listener.class because I only need to use this one.
+	//This is called whenever a packet is received from the server
 	public void received(Connection c, Object p){
-		//Is the received packet the same class as PacketMessage.class?
-		if(p instanceof PacketMessage){
-			//Cast it, so we can access the message within.
-			PacketMessage packet = (PacketMessage) p;
-			System.out.println("received a message from the host: "+packet.message);
-			
-			//We have now received the message!
+		//If the class of the packet is Lobby then do the following
+		if (p instanceof Lobby){
+			Lobby lobby = (Lobby) p;
+			System.out.println("received a message from the host: "+ lobby.lobbyName);
+			System.out.println(lobby.getLobbyName() +" "+ lobby.getRuntime() +" "+ lobby.getState() +" "+ Arrays.toString(lobby.getPlayer()));
 			messageReceived = true;
 		}
 	}
